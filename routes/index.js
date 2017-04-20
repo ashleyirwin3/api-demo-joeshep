0f2ba5ecb90982696425e9ff17f2fa7b97def6ae
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-04-20 14:50:36 -0500

    added index, switched index content to show.js

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,15 @@
-'use strict';
+'use strict'
 
-const { Router } = require('express');
-const router = Router();
+const { Router } = require('express')
+const router = Router()
+router.use(require('./shows'))
 
-const {
-  getShows,
-  getShow,
-  getShowFaves,
-  getShowDirectors,
-  getAllDirex,
-  getDirexShows,
-  addShow,
-  deleteShow } = require('../controllers/showCtrl');
+router.get('/', function (req, res){
+  res.json({
+    "shows": "https://api-demo-ashley-irwin.herokuapp.com/api/v1/shows",
+    "favorites": "https://api-demo-ashley-irwin.herokuapp.com/api/v1/shows/favorites?showId=<show_id>",
+    "directors": "https://api-demo-ashley-irwin.herokuapp.com/api/v1/directors?showId=<show_id>"
+  })
+})
 
-router.get('/shows', getShows);
-router.post('/shows/new', addShow);
-router.get('/shows/favorites', getShowFaves);
-router.get('/shows/directors', getShowDirectors);
-router.get('/directors', getAllDirex)
-router.get('/directors/shows', getDirexShows)
-router.get('/shows/:id', getShow);
-router.delete('/shows/:id', deleteShow);
-
-module.exports = router;
+module.exports= router
--- routes/shows.js
+++ routes/shows.js
@@ -7,13 +7,18 @@ const {
   getShows,
   getShow,
   getShowFaves,
+  getShowDirectors,
+  getAllDirex,
+  getDirexShows,
   addShow,
   deleteShow } = require('../controllers/showCtrl');
 
 router.get('/shows', getShows);
-router.post('/shows/new', addShow)
+router.post('/shows/new', addShow);
 router.get('/shows/favorites', getShowFaves);
 router.get('/shows/directors', getShowDirectors);
+router.get('/directors', getAllDirex)
+router.get('/directors/shows', getDirexShows)
 router.get('/shows/:id', getShow);
 router.delete('/shows/:id', deleteShow);
 
